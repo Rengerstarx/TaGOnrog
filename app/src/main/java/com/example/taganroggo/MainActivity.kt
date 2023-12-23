58bf4be32df22fed4472fcabec73768194ec4ef5
DESKTOP-NHN4PUS\nikit <nikita_prok1705@mail.ru>
2023-12-23 17:54:02 +0300

    merge branchs

--- app/src/main/java/com/example/taganroggo/MainActivity.kt
+++ app/src/main/java/com/example/taganroggo/MainActivity.kt
@@ -1,27 +1,54 @@
 package com.example.taganroggo
 
-import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.taganroggo.databinding.ActivityMainBinding
-import com.google.firebase.FirebaseApp
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.yandex.mapkit.MapKitFactory
 import io.ak1.BubbleTabBar
 import io.ak1.OnBubbleClickListener
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 
 class MainActivity : AppCompatActivity() {
+    private var fl = false
     private lateinit var bubble: BubbleTabBar
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var sensorManager: SensorManager
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
+    private var accelerationValues = FloatArray(3)
+    private var lastAccelerationValues = FloatArray(3)
+    private var shakeThreshold = 30.5f
+    private var minimum_needed_distance = 4.555733555811401E-4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey("187c5f44-6646-457f-b619-eca2dca3cdbe")
+        MapKitFactory.initialize(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        //https://github.com/akshay2211/BubbleTabBar
+
+        registerPermissionListener()
+        checkPermissions()
+
+//https://github.com/akshay2211/BubbleTabBar
         bubble = binding.bubbleTabBar
         replaceFragment(PlaceList())
         bubble.addBubbleListener(object : OnBubbleClickListener {
@@ -39,12 +66,123 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        if (accelerometer != null) {
+            sensorManager.registerListener(
+                sensorEventListener,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        } else {
+// Устройство не поддерживает сенсор ускорения
+
+        }
     }
 
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                System.arraycopy(accelerationValues, 0, lastAccelerationValues, 0, accelerationValues.size)
+                System.arraycopy(event.values, 0, accelerationValues, 0, accelerationValues.size)
+
+                val deltaX = accelerationValues[0] - lastAccelerationValues[0]
+                val deltaY = accelerationValues[1] - lastAccelerationValues[1]
+                val deltaZ = accelerationValues[2] - lastAccelerationValues[2]
+
+                val acceleration = sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ)
+                //Log.i("Dibug1", "${acceleration}")
+                if (acceleration > shakeThreshold) {
+                    isOnPlace()
+                    handleShake()
+                }
+            }
+        }
+    }
     fun replaceFragment(fragment: Fragment){
+        Log.i("Dibug1", "fragment")
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.BAZA,fragment)
         fragmentTransaction.commit()
+    }
+    private fun handleShake() {
+// Код для обработки тряски телефона
+
+    }
+    private fun isOnPlace(){
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+        val task = fusedLocationProviderClient.lastLocation
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        else{
+        }
+        task.addOnSuccessListener {
+            if (it!=null){
+                val latitude = it.latitude
+                val longitude = it.longitude
+                val firebase = FirebaseAPI()
+                firebase.takeAll("Places") {
+                    val places = ParserPLace().parsPalces(it)
+                    var minimum_distance:Double = 100.0
+                    var title:String = ""
+                    for (item in places) {
+                        val distance = calculateDistance(latitude, longitude, item.latitude, item.longitude)
+                        if (minimum_distance>distance){
+                            minimum_distance = distance
+                            title = item.name
+                        }
+                    }
+                    Log.i("info dist", "${title} - ${minimum_distance}")
+                    if (minimum_needed_distance >= minimum_distance){
+                        val fragmentManager = supportFragmentManager
+                        val fragmentTransaction = fragmentManager.beginTransaction()
+                        fragmentTransaction.replace(R.id.BAZA,Map.newInstance())
+                        fragmentTransaction.commit()
+
+                    }
+                }
+            }
+        }
+
+        return
+    }
+    fun checkPermissions() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            pLauncher.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ))
+        }
+    }
+    private fun registerPermissionListener(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+
+        }
+    }
+    fun calculateDistance(x1: Double, y1: Double, x2: Double, y2: Double): Double {
+        val deltaX = x2 - x1
+        val deltaY = y2 - y1
+
+// Используем теорему Пифагора для вычисления расстояния
+        val distance = sqrt(deltaX.pow(2) + deltaY.pow(2))
+        return distance
     }
 }
--- app/src/main/java/com/example/taganroggo/Map.kt
+++ app/src/main/java/com/example/taganroggo/Map.kt
@@ -5,19 +5,231 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.taganroggo.databinding.FragmentMapBinding
+import android.Manifest
+import android.app.Dialog
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PointF
+import android.graphics.drawable.ColorDrawable
+import android.location.Location
+import com.yandex.mapkit.map.Map
+import android.location.LocationManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.telephony.CarrierConfigManager.Gps
+import android.util.Log
+import android.view.Gravity
+import android.view.Window
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKit
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraListener
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.CameraUpdateReason
+import com.yandex.mapkit.map.IconStyle
+import com.yandex.mapkit.map.MapObject
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.mapkit.map.MapObjectTapListener
+import com.yandex.mapkit.map.PlacemarkMapObject
+import com.yandex.mapkit.map.TextStyle
+import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
+import java.lang.Math.pow
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-class Map : Fragment() {
 
-    private lateinit var binding: FragmentMapBinding
+class Map() : Fragment() {
+
+    private lateinit var mapView: MapView
+    private var zoomValue: Float = 16.5f
+    private var startLocation = Point(55.755865, 37.573672)
+    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
+    private lateinit var mapKit: MapKit
+    private lateinit var dialog : Dialog
+    private var placemarkList: ArrayList<PlacemarkMapObject> = arrayListOf()
+    private var name_places : List<String> = listOf(
+        "Газлер",
+        "Каменка",
+        "Чехов сад",
+        "Домик Чехова")
+    var curentLocation: Location? = null
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    //lateinit var pinsCollection : MapObjectCollection
+
+    private val mapObjectTapListener = object : MapObjectTapListener {
+        override fun onMapObjectTap(mapObject: MapObject, point: Point): Boolean{
+            val dialogView = layoutInflater.inflate(R.layout.bottom_sheet_for_map, null)
+            dialog = BottomSheetDialog(requireContext(), R.style.DialogAnimation)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setContentView(dialogView)
+            dialog.getWindow()?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+            dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow()?.getAttributes()?.windowAnimations = R.style.DialogAnimation;
+            dialog.getWindow()?.setGravity(Gravity.BOTTOM);
+            dialog.show()
+            return true
+        }
+    }
+
+    private val mapCameraListener = object : CameraListener{
+        override fun onCameraPositionChanged(
+            map: Map,
+            cameraPosition: CameraPosition,
+            cameraUpdateReason: CameraUpdateReason,
+            finished: Boolean
+        ) {
+            if (finished) { // Если камера закончила движение
+                println(placemarkList.size)
+
+                for (i in 0..3){
+                    println(name_places[i])
+                    val point_coord = placemarkList[i].geometry
+                    val camera_coord = cameraPosition.target
+                    val one = camera_coord.latitude - point_coord.latitude
+                    val two = camera_coord.longitude - point_coord.longitude
+                    val distance = sqrt(one.pow(2)
+                            + two.pow(2))
+                    println(distance)
+                    if (cameraPosition.zoom > 15.0f){
+                        placemarkList[i].setIcon(ImageProvider.fromResource(requireContext(), R.drawable.icon_ex2))
+                    }
+                    else {
+                        placemarkList[i].setIcon(ImageProvider.fromResource(requireContext(), R.drawable.icon_ex))
+                    }
+                }
+                Log.i("Dibug1", "----------------")
+
+                zoomValue = cameraPosition.zoom // После изменения позиции камеры сохраняем величину зума
+            }
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentMapBinding.inflate(inflater,container,false)
+    ): View? {
+        MapKitFactory.initialize(requireContext())
+        mapKit = MapKitFactory.getInstance()
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
 
-        return binding.root
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = activity?.findViewById(R.id.mapview)!!
+
+        val center_button = activity?.findViewById(R.id.center_pos) as Button
+
+        center_button.setOnClickListener {
+            checkPermissions()
+            center_user_position()
+        }
+
+
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+        registerPermissionListener()
+        checkPermissions()
+        center_user_position()
+        //setMarkerInStartLocation()
+        //pinsCollection = mapView.map.mapObjects.addCollection()
+
+        val points = listOf(
+            Point(47.210041, 38.937439),
+            Point(47.213060, 38.938122),
+            Point(47.212494, 38.925883),
+            Point(47.206874, 38.931242),
+        )
+
+        for (i in points) {
+            val placemarkMapObject =
+                mapView.map.mapObjects.addPlacemark(i, ImageProvider.fromResource(requireContext(), R.drawable.icon_ex)) // Добавляем метку со значком
+            placemarkMapObject.addTapListener(mapObjectTapListener)
+            placemarkList.add(placemarkMapObject)
+        }
+        mapView.map.addCameraListener(mapCameraListener)
+
+        var location = mapKit.createUserLocationLayer(mapView.mapWindow)
+        location.isVisible = true
+    }
+
+    private fun moveToStartLocation(p : Point) {
+        mapView.map.move(
+            CameraPosition(p, zoomValue, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 5f),
+            null)
+    }
+
+    private fun center_user_position(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        val task = fusedLocationProviderClient.lastLocation
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        else{
+            Log.i("Dibug1", "1222122121212122112")
+        }
+        task.addOnSuccessListener {
+            if(it!=null){
+                Log.i("Dibug1", "zaebic")
+                val startLoc = Point(it.latitude, it.longitude)
+                moveToStartLocation(startLoc)
+            }
+        }
+        //Log.i("tag", "${user_location}")
+    }
+
+    fun checkPermissions()  {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            pLauncher.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ))
+        }
+    }
+
+    private fun registerPermissionListener(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){
+
+        }
+    }
+
+    companion object {
+        fun newInstance() = Map()
     }
 
 }
