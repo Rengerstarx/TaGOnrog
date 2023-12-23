f88582ad9483c670fd5db0dadec79ce0fafde17f
your_father <Dav.id.bazhe.nov7@gmail.com>
2023-12-24 00:42:29 +0300

    Add registration

--- app/src/main/java/com/example/taganroggo/FirebaseAPI.kt
+++ app/src/main/java/com/example/taganroggo/FirebaseAPI.kt
@@ -62,7 +62,27 @@ class FirebaseAPI {
                 }
             })
     }
-
+    fun writeUser(name: String, mail: String, surname: String, completion: (Int) -> Unit) {
+        FirebaseDatabase.getInstance().getReference("Users")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val count = dataSnapshot.children.count() + 1
+                    FirebaseDatabase.getInstance().getReference("Users").child(count.toString()).child("Mail").setValue(mail)
+                    FirebaseDatabase.getInstance().getReference("Users").child(count.toString()).child("Name").setValue(name)
+                    FirebaseDatabase.getInstance().getReference("Users").child(count.toString()).child("Score").setValue(0)
+                    FirebaseDatabase.getInstance().getReference("Users").child(count.toString()).child("Surname").setValue(surname)
+                    val filenameA = "ava" + (1..5).random() + ".jpg"
+                    val filenameF = "fon" + (1..5).random() + ".jpg"
+                    FirebaseDatabase.getInstance().getReference("Users").child(count.toString()).child("Photo").child("Avatarka").setValue(filenameA)
+                    FirebaseDatabase.getInstance().getReference("Users").child(count.toString()).child("Photo").child("Fon").setValue(filenameF)
+                    completion(count)
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    //Обработка ошибки
+                    println("Failed to read value: ${databaseError.toException()}")
+                }
+            })
+    }
     fun getPicLogo(url: String, completion: (String) -> Unit){
         FirebaseStorage.getInstance().reference.child("$url").downloadUrl.addOnSuccessListener { uri ->
             val imageUrl = uri.toString()
